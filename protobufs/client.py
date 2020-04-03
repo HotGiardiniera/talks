70f30bda2b856269163463df992d15f36ecf6ac2
HotGiardiniera <chris.moirano@gmail.com>
2020-04-03 01:21:36 -0500

    Basic http server and message decoding

--- protobufs/client.py
+++ protobufs/client.py
@@ -1,10 +1,12 @@
 import grpc
+import requests
 
 from pb import messages_pb2_grpc
 from pb.messages_pb2 import MessageRequest, SimpleMessage
 
+SERVER = 'http://localhost:8888'
 
-def run():
+def run_grpc():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = messages_pb2_grpc.SimpleServiceStub(channel)
         message = MessageRequest(
@@ -19,6 +21,17 @@ def run():
         response = stub.HandleMessage(message)
     print("Response recieved received: " + response.type)
 
+def send_http_json():
+    json_payload = {'id': 1, 'names': ['one', 'two', 'three'], 'nums': [1, 2, 3], 'is_json': True}
+    requests.post('http://localhost:8888', json=json_payload)
+
+
+def send_http_protobuf():
+    s = SimpleMessage(id=1, names=['one', 'two', 'three'], nums=[1,2,3], is_json=False)
+    headers={'content-type': 'protobuf'}
+    requests.post(SERVER, data={'data': s.SerializeToString()}, headers=headers)
+
 
 if __name__ == "__main__":
-    run()
+    send_http_json()
+    send_http_protobuf()
--- protobufs/server.py
+++ protobufs/server.py
@@ -1,3 +1,9 @@
+import json
+import sys
+import socketserver
+import urllib
+from http.server import SimpleHTTPRequestHandler
+
 import grpc
 from grpc_reflection.v1alpha import reflection
 
@@ -38,7 +44,8 @@ def read_message_from_file(filename: str):
     return proto_obj
 
 
-def serve():
+def grpc_serve():
+    print("Starting grpc server port 50051...")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     messages_pb2_grpc.add_SimpleServiceServicer_to_server(SimpleServiceServicer(), server)
     SERVICE_NAMES = (
@@ -51,6 +58,51 @@ def serve():
     server.wait_for_termination()
 
 
+class Handler(SimpleHTTPRequestHandler):
+
+
+    def _decode_protobuf(self, body):
+        protobuf_message = urllib.parse.unquote(body.decode().split('=')[1]).encode()  # Ugly TODO comments and break down
+        proto_obj = SimpleMessage()
+        proto_obj.ParseFromString(protobuf_message)
+        print("GOT A PROTOBUF MESSAGE!", proto_obj.id, proto_obj.names)
+
+    def _handle_json(self, body):
+        decoded_body = json.loads(body.decode())
+        print("GOT A JSON MESSAGE!", decoded_body)
+
+
+    def do_POST(self):
+        # Construct a server response.
+        content_length = int(self.headers['Content-Length'])
+        post_body = self.rfile.read(content_length)
+        if self.headers.get('Content-Type', '') == 'protobuf':
+            self._decode_protobuf(post_body)
+        else:
+            self._handle_json(post_body)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        # Send the html message
+        return_string = f'Recieved {post_body}\nSize{content_length}\n'
+        self.wfile.write(return_string.encode())
+
+
+def http_serve():
+    print("Starting http server port 8888...")
+    httpd = socketserver.TCPServer(('', 8888), Handler)
+    httpd.serve_forever()
+
+
 if __name__ == "__main__":
-    print("Starting the grpc server...")
-    serve()
+    argv = sys.argv[1:]
+    SERVER_MAP = {
+        'http': http_serve,
+        'grpc': grpc_serve,
+    }
+
+    if len(argv) < 1:
+        print("Need to specify server as program input (http, grpc)")
+        exit(-1)
+
+    server = SERVER_MAP.get(argv[0], grpc_serve)()
